e2c0bdc9a4b31ff54907efeb4edea35c41d55fd3
hkc <starbfly0915@gmail.com>
2020-07-24 23:09:18 +0300

    More imports fixes

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-import flask
 import json
 from os import environ
+
+import flask
+import telebot
+
 from mainapp import bot
+
 
 WEBHOOK_HOST = environ.get('WEBHOOK_HOST')
 WEBHOOK_PATH = environ.get('WEBHOOK_PATH')
 
 
 app = flask.Flask(__name__)
+
 
 @app.route(WEBHOOK_PATH, methods=['POST'])
 def webhook():
